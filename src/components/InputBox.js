2138e8ac85b5c4a41d1b4316b35a8a30506b4988
noparamin <noparamin@naver.com>
2022-01-26 18:42:05 +0900

    enter 눌렀을 때 메뉴 추가

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -4,21 +4,38 @@ function InputBox({ menuList, setMenuList }) {
     const [menuName, setMenuName] = useState("");
 
     const onChangeInput = (e) => {
-        setMenuName(e.target.value)
+        setMenuName(e.target.value);
     };
 
-    const onClickAdd = () => {
+    const addMenuName = (e) => {
         if(menuName === "") {
             return;
         }
+        e.preventDefault();
         setMenuList((currentArray) => [...currentArray, menuName]);
         setMenuName("");
     };
+
+    const onEnterCheck = (e) => {
+        if(e.key === "Enter") {
+            addMenuName(e);
+        }
+    };
+
+    const onClickAdd = (e) => {
+        addMenuName(e);
+    };
+    
     return (
         <div className="menu-input-box">
-            <label hidden className="label">메뉴 이름</label>
+            <form>
+                <label hidden className="label">메뉴 이름</label>
+                <input type="text" placeholder="메뉴 이름" className="input" value={menuName} onChange={onChangeInput} onKeyPress={onEnterCheck}></input>
+                <button type="submit" className="menu-input-box-add-btn" onClick={onClickAdd}>확인</button>
+            </form>
+            {/* <label hidden className="label">메뉴 이름</label>
             <input type="text" placeholder="메뉴 이름" className="input" value={menuName} onChange={onChangeInput}></input>
-            <button type="submit" className="menu-input-box-add-btn" onClick={onClickAdd}>확인</button>
+            <button type="submit" className="menu-input-box-add-btn" onClick={onClickAdd}>확인</button> */}
         </div>
     );
 }
